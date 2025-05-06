b259e30dd6b12545e04883f734b5fc2d4d5a7352
Lara Pla <laraplamoreno@gmail.com>
2025-05-06 17:27:39 +0200

    chore: build code structure

--- Quiz.java
+++ Quiz.java
@@ -3,10 +3,13 @@ public class Quiz {
         //Escribe un programa que sea un pequeño juego trivial de acertar preguntas
         //Cada pregunta tendrá 4 opciones que el usuario podrá escoger
         //Por cada respuesta correcta, añade 5 puntos al puntaje
+
         //Al finalizar el juego imprime cual fue tu puntaje /20
+
         //Si el puntaje es 15 o más, imprime: Tú si que sabes mucho
         //Si el puntaje es menor de 15 pero mayor o igual a 5, imprime: No lo has hecho mal
         //De lo contrario imprime: Buena suerte la próxima vez
+
         //Las opciones a, b, c y d de cada pregunta, deberán dejar en terminal un espacio de sangría, averigua como hacerlo en java
 
         //Preguntas:
@@ -36,6 +39,39 @@ public class Quiz {
         */
 
         //Respuestas: 1. b / 2. c / 3. b y d / 4. a
+
+
+        System.out.println("1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?");
+        String answers1 = "a. Ada Lovelace\n" +
+                "b. Jude Milhon\n" +
+                "c. Martha Ackelsberg\n" +
+                "d. Jane Jacobs";
+        System.out.println(answers1.indent(4));
+
+        //scanner
+        System.out.println("\n2. ¿Quién es Fuencisla Clemares?");
+        String answers2 = "a. La directora general de IBM Europa, Oriente Medio y África\n" +
+                "b. La vicepresidenta de Microsoft Western Europe\n" +
+                "c. La directora general de Google en España y Portugal\n" +
+                "d. Fundadora y CEO de Marsi Bionics";
+        System.out.println(answers2.indent(4));
+
+        System.out.println("\n3. ¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr?");
+        String answers3 = "a. Primera programadora de software\n" +
+                "b. Una actriz de cine\n" +
+                "c. Inventora del procesador de datos\n" +
+                "d. Inventora del sistema de comunicaciones de salto de frecuencia, base del wifi, bluetooth y GPS";
+        System.out.println(answers3.indent(4));
+
+        System.out.println("\n4. ¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?");
+        String answers = "a. Katherine Johnson, Dorothy Vaughan y Mary Jackson\n" +
+                "b. Jessica Miller, Emily Johnson, Sarah Davis\n" +
+                "c. bell hooks, Toni Morrison y Alice Walker\n" +
+                "d. Emmeline Pankhurst, Millicent Fawcett y Harriet Harman";
+        System.out.println(answers.indent(4));
+
+
+
 
     }
     
